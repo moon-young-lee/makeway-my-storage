b22ea6cdd8f3d75f614a7988dc25027474a41855
moony <62280843+moon-young-lee@users.noreply.github.com>
2020-04-21 10:53:13 +0900

    전반적인 틀

---
+++ foot.php
@@ -0,0 +1,56 @@
+ </section>
+  <footer>
+	<div class="wrap">
+		<div class="col1 col">
+			<img src="img/logo_foot.png" alt="">
+			<ul class="input_flex">
+				<li>회사소개</li>
+				<li>개인정보처리방침</li>
+				<li>이용약관</li>
+				<li>오시는길</li>
+			</ul>
+			<p>대표 : 김영숙  |  인천광역시 미추홀구 경인로229 인천IT타워 412호<br>
+			문의:032)710-0825  |  FAX:032)232-1825<br>
+			업무시간:평일 09:00~18:00, 주말·공휴일 휴무<br>
+			mkw@makeway.co.kr  |   사업자등록번호: 748-53-00262<br><br>
+			Copyrightⓒ2020 MAKEWAY. All right reserved.
+			</p>
+		</div>
+		<div class="col2 col">
+			<div>간편견적문의<span class="col2_smallTxt">상세 견적문의는 전화 또는 여기를 클릭하세요.</span></div>
+			<form action="">
+				<div class="input_flex">
+					<div class="input_txt">
+						<input type="text" placeholder="이름(고객성함)">
+					</div>
+					<div class="input_txt">
+						<input type="text" placeholder="휴대폰번호">
+					</div>
+				</div>
+				<div class="input_flex">
+					<div class="input_txt">
+						<input type="text" placeholder="이메일">
+					</div>
+					<div class="input_txt">
+						<input type="text" placeholder="참고사이트">
+					</div>
+				</div>
+				<div>
+					<textarea placeholder="문의내용" class="input_txt" name="" id="" cols="30" rows="10"></textarea>
+				</div>
+			</form>
+		</div>
+		<div class="col3 col">
+			<div class="map">
+				
+			</div>
+			<div class="location_txt">
+				<i class="fas fa-map-marker-alt"></i>
+				<div>Location</div>
+				<p>인천광역시 미추홀구<br>경인로229 인천IT타워 412호</p>
+			</div>
+		</div>
+	</div>
+  </footer>
+ </body>
+</html>
---
+++ head.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link href="/common/css/m.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+  <script src="https://use.fontawesome.com/releases/v5.2.0/js/all.js"></script>
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css"> -->
+  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script> -->
+  <!-- <script src="https://code.jquery.com/jquery-3.5.0.js"
+  integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc="
+  crossorigin="anonymous"></script> -->
+  <title>Document</title>
+
+ </head>
+ <body>
+
+  <section class="sec1">
+  <header>
+	<img src="img/logo.png" alt="">
+	<nav>
+		<ul class="m_nav">
+			<li>웹솔루션제작
+				<!-- <ul class="s_nav">   
+					<li>서비스소개</li>	
+					<li>제작솔루션</li>	
+					<li>주요기능</li>	
+					<li>구매문의</li>	
+				</ul> -->
+			</li>
+			<li>웹프로그램개발
+				<!-- <ul class="s_nav">
+					<li>서비스소개</li>
+					<li>제작솔루션</li>	
+					<li>주요기능</li>	
+					<li>구매문의</li>	
+				</ul> -->
+			</li>
+			<li>고객센터
+				<!-- <ul class="s_nav">
+					<li>1:1상담</li>	
+					<li>공지사항</li>	
+					<li>자주묻는질문</li>	
+				</ul>	 -->
+			</li>
+			<li>다운로드
+				<!-- <ul class="s_nav">
+					<li>다운로드</li>
+				</ul> -->
+			</li>
+			<li>회사소개
+				<!-- <ul class="s_nav">
+					<li>회사소개</li>
+					<li>인증및지적재산권</li>
+					<li>오시는길</li>
+				</ul> -->
+			</li>
+		</ul>
+	</nav>
+	<div class="loginBox">
+		<ul>
+			<li><i class="fas fa-home"></i>홈</li>
+			<span class="login_line"></span>
+			<li><i class="fas fa-sign-in-alt"></i>로그인</li>
+			<span class="login_line"></span>
+			<li><i class="fas fa-user-plus"></i>회원가입</li>
+		</ul>
+		<div class="sns">
+			<span><img src="img/icon_kakao.png" alt=""></span>
+			<span><img src="img/icon_blog.jpg" alt=""></span>
+		</div>
+	</div>
+	
+  </header>
+	<div style="position:relative; width: 100%; height: 700px; overflow: hidden;">
+		<?php
+		$banner_code="main_visual"; 
+		include $_SERVER['DOCUMENT_ROOT']."/common/banner/banner_visual.php";
+		?> 
+	</div>
+
+ <!--  <div class="main_banner">
+	<img src="img/main03-1.jpg" alt="">
+  </div>
+ -->
